908d16c4607df622159a685117ce1e9c7a1b6ddb
Max Gemereth <177759368+gemerethmax@users.noreply.github.com>
2025-05-13 16:44:32 -0400

    styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,16 +4,17 @@ import redditLogo from '../assets/Reddit_logo.png';
 const NavBar = () => {
 
     return(
-        <header>
-            <img height="30px" width="30px" src={redditLogo} alt="Reddit Logo"/><h1>RedditMinimal</h1>
-            <nav>
+        <header >
+            <nav class="width-full flex justify-between items-center bg-blue-400 p-4">
+                <img height="30px" width="30px" src={redditLogo} alt="Reddit Logo"/><h1>RedditMinimal</h1>
+                <label htmlFor="search">Search</label>
+                <input id="search" type="text" placeholder="Search..." />
                 <ul>
                    <NavLink to="/"><li>Home</li></NavLink>
                    <NavLink to="/about"><li>About</li></NavLink>
                    <NavLink to="/contact"><li>Contact</li></NavLink>
                 </ul>
-                <label htmlFor="search">Search</label>
-                <input id="search" type="text" placeholder="Search..." />
+                
             </nav>
 
 
